6d3f8ebebf6e5091485cc580f7c66c5283a69ca9
andrewwpalominoo <andres@palominocassain.com>
2026-01-28 20:29:33 +0100

    .

--- SWING/ejerciciosPREexamen/ej5/src/main/java/es/cide/prog/Main.java
+++ SWING/ejerciciosPREexamen/ej5/src/main/java/es/cide/prog/Main.java
@@ -12,9 +12,24 @@ public class Main {
         finestra.setSize(400,400);
         finestra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         finestra.setLayout(new FlowLayout());
-        JTextField textField = new JTextField(20);
+        JTextField num1 = new JTextField();
+        JTextField num2 = new JTextField();
+
         JButton sumarButton = new JButton("Sumar");
 
+        try {
+            sumarButton.addActionListener(e -> {
+                int n1 = Integer.parseInt(num1.getText());
+                int n2 = Integer.parseInt(num2.getText());
+                int suma = n1 + n2;
+                System.out.println("La suma es: " + suma);
+            });
+        } catch (NumberFormatException e) {
+            System.out.println("Error");
+        }
+        finestra.add(num1);
+        finestra.add(num2);
+        finestra.add(sumarButton);
         finestra.setVisible(true);
     }
 }
